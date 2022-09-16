4316614fdb3cf199dfbb195111262f21e28c3e77
smellhyang <dlgiddk10@gmail.com>
2022-09-16 19:31:05 +0900

    Initial commit

---
+++ src/test/java/com/smell/url/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smell.url;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
